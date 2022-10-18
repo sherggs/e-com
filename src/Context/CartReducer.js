bfe1c438d1bcfed68fec61969361080380aecf39
Ademola <sherggs@gmail.com>
2022-10-18 19:26:58 +0100

    reducer case ADD

---
+++ src/Context/CartReducer.js
@@ -0,0 +1,22 @@
+export const CartReducer = (state, action) => {
+    debugger;
+
+switch(action.type){
+    case "ADD":
+        const index = state.cartItems.findIndex(x => x.id === action.payload.id);
+
+        if(index === -1){
+            state.cartItems.push({...action.payload, quantity: 1})
+        }
+        else{
+            state.cartItems[index].quantity++;
+        }
+    case "REMOVE":
+        return state;
+
+
+    default:
+        return state;
+}
+
+}
