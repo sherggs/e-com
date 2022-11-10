daa9828fc0cd79b0f673a41aa9e509d2e3e090e7
Ademola <sherggs@gmail.com>
2022-11-10 02:26:47 +0000

    category try catch

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
-import { CartContext } from '../Context/CartContext';
+// import { CartContext } from '../Context/CartContext';
 import { getCategoryById } from './Fetcher';
 
 
